021dcf640bf327838097713544b82faaf8b881a7
cash <cash@cashwu.com>
2016-06-11 23:45:56 +0800

    coding

---
+++ kata0611_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,35 @@
+﻿namespace kata0611_FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        public string GetValue(int input)
+        {
+            var result = string.Empty;
+            
+            if (IsDivided3(input))
+            {
+                result += "Fizz";
+            }
+            if (IsDivided5(input))
+            {
+                result += "Buzz";
+            }
+            if (string.IsNullOrEmpty(result))
+            {
+                result = input.ToString();
+            }
+
+            return result;
+        }
+
+        private static bool IsDivided5(int input)
+        {
+            return input % 5 == 0;
+        }
+
+        private static bool IsDivided3(int input)
+        {
+            return input % 3 == 0;
+        }
+    }
+}
---
+++ kata0611_FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace kata0611_FizzBuzz
+{
+    [TestFixture]
+    class FizzBuzzTest
+    {
+        private readonly FizzBuzz _fizzBuzz = new FizzBuzz();
+
+        [Test]
+        public void NormalNumber_return_Default([Values(1, 2, 4, 7, 8, 11, 13, 14)]int input)
+        {
+            Assert.AreEqual(input.ToString(), _fizzBuzz.GetValue(input));
+        }
+
+        [Test]
+        public void Divided_3_number_return_Fizz([Values(3, 6, 9, 12)]int input)
+        {
+            Assert.AreEqual("Fizz", _fizzBuzz.GetValue(input));
+        }
+
+        [Test]
+        public void Divided_5_number_return_Buzz([Values(5, 10)]int input)
+        {
+            Assert.AreEqual("Buzz", _fizzBuzz.GetValue(input));
+        }
+
+        [Test]
+        public void Divided_3_and_5_number_return_FizzBuzz([Values(15, 30, 45, 60, 75, 90)]int input)
+        {
+            Assert.AreEqual("FizzBuzz", _fizzBuzz.GetValue(input));
+        }
+    }
+}
